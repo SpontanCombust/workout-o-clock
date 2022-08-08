3a18fe8b8f557da02cfeb5fdf17c02f5eee00a73
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2022-08-08 21:58:18 +0200

    simplify context code

--- src/context/WorkoutContext.tsx
+++ src/context/WorkoutContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useState } from "react";
 
 import { WorkoutTask } from "../types/WorkoutTask";
 
@@ -31,122 +31,62 @@ const initialState: WorkoutContextProps = {
     setTaskOrder: () => {},
 };
 
-export const WorkoutContext = createContext(initialState);
-
-
-
-interface WorkoutReducerActionAddTask {
-    type: "ADD_TASK";
-    task: WorkoutTask;
-}
-interface WorkoutReducerActionAddTaskMultiple {
-    type: "ADD_TASK_MULTIPLE";
-    tasks: WorkoutTask[];
-}
-interface WorkoutReducerActionUpdateTask {
-    type: "UPDATE_TASK";
-    uuid: string;
-    task: WorkoutTask;
-}
-interface WorkoutReducerActionRemoveTask {
-    type: "REMOVE_TASK";
-    uuid: string;
-}
-interface WorkoutReducerActionClearTasks {
-    type: "CLEAR_TASKS";
-}
-
-interface WorkoutReducerActionSetTaskOrder {
-    type: "SET_TASK_ORDER";
-    order: string[];
-}
-
-type WorkoutReducerAction = 
-    WorkoutReducerActionAddTask         |
-    WorkoutReducerActionAddTaskMultiple |
-    WorkoutReducerActionUpdateTask      |
-    WorkoutReducerActionRemoveTask      |
-    WorkoutReducerActionClearTasks      |
-    WorkoutReducerActionSetTaskOrder    ;
-
-function WorkoutReducer(state: WorkoutContextProps, action: WorkoutReducerAction): WorkoutContextProps {
-    switch (action.type) {
-        //TODO check for duplicates
-        case "ADD_TASK":
-            if(state.tasks.find(task => task.uuid === action.task.uuid) !== undefined) {
-                return {
-                    ...state,
-                }
-            } else {
-                return {
-                    ...state, 
-                    tasks: [...state.tasks, action.task],
-                    taskOrder: [...state.taskOrder, action.task.uuid],
-                };
-            }
-        case "ADD_TASK_MULTIPLE":
-            const validTasks = action.tasks.filter(task => state.tasks.find(t => t.uuid === task.uuid) === undefined);
-            return {
-                ...state,
-                tasks: [...state.tasks, ...validTasks],
-                taskOrder: [...state.taskOrder, ...validTasks.map(task => task.uuid)],
-            };
-        case "UPDATE_TASK":
-            const i = state.tasks.findIndex(task => task.uuid == action.uuid);
-            if(i != -1) {
-                state.tasks[i] = action.task;
-            }
-            return state;
-        case "REMOVE_TASK":
-            return {
-                ...state, 
-                tasks: state.tasks.filter(task => task.uuid !== action.uuid),
-                taskOrder: state.taskOrder.filter(uuid => uuid !== action.uuid)
-            };
-        case "CLEAR_TASKS":
-            return {
-                ...state,
-                tasks: [],
-                taskOrder: [],
-            }
-        case "SET_TASK_ORDER":
-            const validIds = action.order.filter(uuid => state.tasks.find(task => task.uuid == uuid) !== undefined);
-            return {
-                ...state,
-                taskOrder: validIds
-            }
-        default:
-            return state;
-    }
-}
-
-
+export const WorkoutContext = createContext<WorkoutContextProps>(initialState);
 
 
 export function WorkoutContextProvider(props: {children: any}) {
-    const [state, dispatch] = useReducer(WorkoutReducer, initialState);
+    const [state, setState] = useState(initialState);
 
     function addTask(task: WorkoutTask) {
-        dispatch({type: "ADD_TASK", task: task});
+        if(state.tasks.find(t => t.uuid === task.uuid) === undefined) {
+            setState({
+                ...state, 
+                tasks: [...state.tasks, task],
+                taskOrder: [...state.taskOrder, task.uuid],
+            });
+        }
     }
     function addTaskMultiple(tasks: WorkoutTask[]) {
-        dispatch({type: "ADD_TASK_MULTIPLE", tasks: tasks});
+        const validTasks = tasks.filter(pendingTask => state.tasks.find(t => t.uuid === pendingTask.uuid) === undefined);
+        setState({
+            ...state,
+            tasks: [...state.tasks, ...validTasks],
+            taskOrder: [...state.taskOrder, ...validTasks.map(task => task.uuid)],
+        });
     }
     function updateTask(uuid: string, task: WorkoutTask) {
-        dispatch({type: "UPDATE_TASK", uuid: uuid, task: task});
+        const i = state.tasks.findIndex(task => task.uuid == uuid);
+        if(i != -1) {
+            setState({
+                ...state,
+                tasks: [...state.tasks.slice(0, i), task, ...state.tasks.slice(i + 1)],
+            });
+        }
     }
     function removeTask(uuid: string) {
-        dispatch({type: "REMOVE_TASK", uuid: uuid});
+        setState({
+            ...state,
+            tasks: state.tasks.filter(task => task.uuid !== uuid),
+            taskOrder: state.taskOrder.filter(id => id !== uuid),
+        });
     }
     function clearTasks() {
-        dispatch({type: "CLEAR_TASKS"});
+        setState({
+            ...state,
+            tasks: [],
+            taskOrder: [],
+        });
     }
     function findTask(uuid: string): WorkoutTask | undefined {
         return state.tasks.find(task => task.uuid == uuid);
     }
 
     function setTaskOrder(order: string[]) {
-        dispatch({type: "SET_TASK_ORDER", order: order});
+        const validIds = order.filter(uuid => state.tasks.find(task => task.uuid == uuid) !== undefined);
+        setState({
+            ...state,
+            taskOrder: validIds,
+        });
     }
 
 
