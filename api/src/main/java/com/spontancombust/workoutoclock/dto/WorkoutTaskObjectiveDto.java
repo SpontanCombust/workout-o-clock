cc4eca26c67cb27decc518f7d37342134c2483c1
SpontanCombust <cedro_p@o2.pl>
2024-11-16 22:26:10 +0100

    fixed `WorkoutTaskObjectiveDto` deserialization

--- api/src/main/java/com/spontancombust/workoutoclock/dto/WorkoutTaskObjectiveDto.java
+++ api/src/main/java/com/spontancombust/workoutoclock/dto/WorkoutTaskObjectiveDto.java
@@ -1,5 +1,9 @@
 package com.spontancombust.workoutoclock.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +12,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonTypeInfo(use = Id.NAME, include = As.EXISTING_PROPERTY, property = "objectiveType", visible = true)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = WorkoutTaskTimeObjectiveDto.class, name = "T"),
+    @JsonSubTypes.Type(value = WorkoutTaskRepsObjectiveDto.class, name = "R")
+})
 public abstract class WorkoutTaskObjectiveDto {
     private String objectiveType;
 }
